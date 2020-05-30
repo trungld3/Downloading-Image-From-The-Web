52d9baea3ff44b5c2285c6d812740849de82fa13
trungld3 <k21tpm4@gmail.com>
2020-05-30 07:29:43 +0700

    Initial Commit

---
+++ Downloading Image From The Web/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Downloading Image From The Web
+//
+//  Created by TrungLD on 5/30/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
